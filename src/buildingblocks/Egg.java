7aed6f4d92d47c8e423957ca5c11137284905a55
Ahmet Eroglu <aeroglu2016@gmail.com>
2019-10-23 20:15:08 +0200

    add egg class

--- src/buildingblocks/Egg.java
+++ src/buildingblocks/Egg.java
@@ -1,10 +1,16 @@
 package buildingblocks;
 
 public class Egg {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public Egg() {
+		number = 5;
 	}
-
+	
+	public static void main(String[] args) {
+		Egg egg = new Egg();
+		System.out.println(egg.number);
+	}
+	
+	private int number = 3;
+	{ number = 4;}
 }
