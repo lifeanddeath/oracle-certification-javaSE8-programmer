ebfd78043c8142d48ccd34834ed553b1d8d9d9ed
Ahmet Eroglu <aeroglu2016@gmail.com>
2019-11-01 20:22:47 +0100

    add nestedloops class example

---
+++ src/operators_and_statements/NestedLoops.java
@@ -0,0 +1,17 @@
+package operators_and_statements;
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+		int[][] myComplexArray = { { 5, 2, 1, 3 }, { 3, 9, 8, 9 }, { 5, 7, 12, 7 } };
+		
+		for (int[] mySimpleArray : myComplexArray) {
+			for (int i = 0; i < mySimpleArray.length; i++) {
+				System.out.print(mySimpleArray[i] + "\t");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
