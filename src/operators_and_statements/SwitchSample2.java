1435626cac8a4ba1e11c2522908f58276a96a520
Ahmet Eroglu <aeroglu2016@gmail.com>
2019-11-03 10:58:42 +0100

    add switchsample2 class example

---
+++ src/operators_and_statements/SwitchSample2.java
@@ -0,0 +1,22 @@
+package operators_and_statements;
+
+public class SwitchSample2 {
+
+	public static void main(String[] args) {
+		int a = 5;
+
+		switch (a) {
+		case 3:
+			System.out.println("3");
+		case 5:
+			System.out.println("5");
+		case 6:
+			System.out.println("6");
+			break;
+		case 9:
+			System.out.println("3");
+		}
+
+	}
+
+}
