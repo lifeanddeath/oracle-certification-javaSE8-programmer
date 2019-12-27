45528c0773c13dd8361768898d351d8242b6cf96
Ahmet Eroglu <aeroglu2016@gmail.com>
2019-12-27 20:13:05 +0100

    add examples for overriding static and instance methods

---
+++ src/overriding_exercises/Animal.java
@@ -0,0 +1,14 @@
+package overriding_exercises;
+
+public class Animal {
+	
+	int someNumber = 10;
+	
+	 static void testClassMethod() {
+		System.out.println("this is a method in Animal Class");
+	}
+	
+     void testInstanceMethod() {
+    	 System.out.println("this is a instance method in Animal Class");
+     }
+}
---
+++ src/overriding_exercises/Cat.java
@@ -0,0 +1,39 @@
+package overriding_exercises;
+
+public class Cat extends Animal{
+	
+	int someNumber = 20;
+	
+	static void testClassMethod() {
+		System.out.println("this method is from cat class");
+	}
+	
+	void testInstanceMethod() {
+		System.out.println("this method is from cat class");
+	}
+
+	public static void main(String[] args) {
+		
+		Animal animal = new Cat();
+		Cat cat = new Cat();
+		
+		animal.testClassMethod(); // prints animal
+		animal.testInstanceMethod(); //prints cat
+		Animal.testClassMethod(); // prints animal
+		System.out.println(animal.someNumber); //prints 10
+		
+		cat.testClassMethod();     //prints cat
+		cat.testInstanceMethod();  //prints cat
+		System.out.println(cat.someNumber); // prints 20
+		 
+		System.out.println(new Cat().someNumber); // prints 20
+		new Cat().testClassMethod(); // prints cat
+		new Cat().testInstanceMethod(); // prints cat
+		
+	//	Cat castedCat = (Cat) new Animal(); // gives class cast exception because of overriding
+	//	castedCat.testClassMethod();    
+	//	castedCat.testInstanceMethod();  
+		
+	}
+
+}
