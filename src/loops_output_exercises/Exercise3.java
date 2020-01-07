fc9b9a244c11b351fb48aa5de0263948c672e693
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-01-07 20:52:20 +0100

    add some char to short conversion

---
+++ src/loops_output_exercises/Exercise3.java
@@ -0,0 +1,12 @@
+package loops_output_exercises;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		short s = -1;
+		char c = (char) s;
+		
+		System.out.println(c);
+	}
+
+}
