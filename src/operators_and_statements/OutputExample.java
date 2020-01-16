8321880bb6749d332d884bc8f3387becdf169d99
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-01-16 22:06:59 +0100

    add an output example

---
+++ src/operators_and_statements/OutputExample.java
@@ -0,0 +1,13 @@
+package operators_and_statements;
+
+public class OutputExample {
+
+	public static void main(String[] args) {
+		Byte b = 1;
+		Integer c = 1;
+		
+	//	System.out.println(b == c); // wouldn't compile but primitive would return true
+
+	}
+
+}
