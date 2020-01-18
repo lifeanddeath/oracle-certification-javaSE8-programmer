b55a30fd221d7a533b87db7040b070b82cdc5e26
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-01-18 21:11:35 +0100

    add example for primitive comparison

---
+++ src/primitives/Example3.java
@@ -0,0 +1,14 @@
+package primitives;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		short s = 2;
+		char  c = 2;
+		
+		
+		System.out.println( s == c );  //prints true
+
+	}
+
+}
