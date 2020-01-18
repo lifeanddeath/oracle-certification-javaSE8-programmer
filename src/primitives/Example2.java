115bc4884b9efef735ba857503b91be273d2df2f
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-01-18 15:57:07 +0100

    add new example with wrapper classes comparison

---
+++ src/primitives/Example2.java
@@ -0,0 +1,14 @@
+package primitives;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Integer i = new Integer(42);
+		Long l = new Long(42);
+		Double d = new Double(42.0);
+		
+	    System.out.println(i.equals(d)); // prints false
+
+	}
+
+}
