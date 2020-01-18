85aa93ee06fb4bdde83a1361636c4d6751136790
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-01-18 15:53:50 +0100

    fix compilation error

--- src/operators_and_statements/OutputExample.java
+++ src/operators_and_statements/OutputExample.java
@@ -6,7 +6,7 @@ public class OutputExample {
 		Byte b = 1;
 		Integer c = 1;
 		
-	//	System.out.println(b == c); // wouldn't compile but primitive would return true
+	//	System.out.println(be == ce); // wouldn't compile but primitive would return true
 
 	}
 
