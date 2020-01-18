cf57cb9fa01056351691164b855444e880faf3dc
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-01-18 20:59:23 +0100

    add example for default constructor

---
+++ src/overriding_exercises/DefaultConstructorExample.java
@@ -0,0 +1,18 @@
+package overriding_exercises;
+
+public class DefaultConstructorExample {
+	
+	int a;
+	int b;
+	
+	public DefaultConstructorExample(int x, int y) {
+		this.a = x;
+		this.b = y;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/overriding_exercises/DefaultConstructorExample2.java
@@ -0,0 +1,15 @@
+package overriding_exercises;
+
+public class DefaultConstructorExample2 extends DefaultConstructorExample {
+
+	public DefaultConstructorExample2(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
