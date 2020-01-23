bd484d32ed5c4f64259a60aa3309a8f69e538d8a
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-01-23 19:26:38 +0100

    add an example to output of loops

---
+++ src/loops_output_exercises/DuplicateVariable.java
@@ -0,0 +1,16 @@
+package loops_output_exercises;
+
+public class DuplicateVariable {
+
+	static int  x = 10;
+	public static void main(String[] args) {
+		
+		for (int x = 0; x<3 ; x++) {
+			System.out.println(x);
+		}
+		
+		for (; ;)
+			System.out.println(3);
+	}
+
+}
