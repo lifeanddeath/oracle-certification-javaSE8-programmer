61ae6acd4b51ae3834b00163332a8dad470e9e14
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-02-05 22:16:48 +0100

    add new example for primitives

---
+++ src/primitives/Example6.java
@@ -0,0 +1,15 @@
+package primitives;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		
+		 String[] arr = new String[]{"a","b","c","d"};
+		
+		String b = "d";
+		switch(b) {
+		case arr[0]: 
+		}	
+	}
+
+}
