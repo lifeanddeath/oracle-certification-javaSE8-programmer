37e12d8add9af68b9a576571841363358e8e37b7
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-02-08 10:22:21 +0100

    add new example with switch statement

---
+++ src/primitives/Example9.java
@@ -0,0 +1,14 @@
+package primitives;
+
+public class Example9 {
+
+	// doesn't compile because it should be compile time constant
+	public static void main(String[] args) {
+		String[] b = new String[] {"a","b","c"};
+		
+		switch("c") {
+		case b[0]: System.out.println("hi");
+		}
+	}
+
+}
