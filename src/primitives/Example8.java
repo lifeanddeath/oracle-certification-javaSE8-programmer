9f5b7ebcab6ca3ed00170e1af3ce93b886c75bce
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-02-08 10:18:33 +0100

    add new example with switch statement

---
+++ src/primitives/Example8.java
@@ -0,0 +1,16 @@
+package primitives;
+
+public class Example8 {
+
+	public static void main(String[] args) {
+		String  a = "a";
+		String b = "s";
+		
+		switch (a) {
+		case b: System.out.println("first case");
+		case b+1: System.out.println("first case");
+
+	}
+	}
+
+}
