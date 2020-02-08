f6aff59fabee83ad9115d0899a19ff2191ea2b19
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-02-08 09:09:53 +0100

    add new array exercise

---
+++ src/array_exercises/Exercise2.java
@@ -0,0 +1,13 @@
+package array_exercises;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		String[][] strs = new String[8][];
+		
+		strs[1] = new String[2];
+		
+		System.out.println(strs[1][1]);
+	}
+
+}
