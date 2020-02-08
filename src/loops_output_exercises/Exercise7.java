6f864efa9e582a256b6541c415113c18f44aa037
Ahmet Eroglu <aeroglu2016@gmail.com>
2020-02-08 09:10:15 +0100

    add new while loop example

---
+++ src/loops_output_exercises/Exercise7.java
@@ -0,0 +1,17 @@
+package loops_output_exercises;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+		int i  = 0;
+		
+		while (i++ < 10 ) {
+			
+		}
+		
+		String out = i > 10 ? ">" : "<";
+		
+		System.out.println("10" + out + i);
+	}
+
+}
